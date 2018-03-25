3a736a5dd9b9652ef86f7ba1cd9d026b985d5e2a
Howard Wu <howardwu@berkeley.edu>
2018-03-25 17:25:02 +0200

    Update variable naming

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 var LibraToken = artifacts.require("./LibraToken.sol");
-var Crowdsale = artifacts.require("./LibraTokenSol.sol");
+var LibraTokenSale = artifacts.require("./LibraTokenSol.sol");
 
 module.exports = function (deployer) {
     deployer.deploy(LibraToken).then(function () {
-        return deployer.deploy(Crowdsale, 800, "0x627306090abaB3A6e1400e9345bC60c78a8BEf57", LibraToken.address);
+        return deployer.deploy(LibraTokenSale, 800, "0x627306090abaB3A6e1400e9345bC60c78a8BEf57", LibraToken.address);
     });
 };
