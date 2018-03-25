bae0fe5865ddb550f23eb3e2abd4d257c98bda89
Ali Mousa <amousa11@gmail.com>
2018-03-25 13:22:08 +0200

    linting, gitignore

--- contracts/LibraToken.sol
+++ contracts/LibraToken.sol
@@ -12,19 +12,19 @@ import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
  */
 contract LibraToken is StandardToken {
 
-  string public constant name = "LibraToken"; // solium-disable-line uppercase
-  string public constant symbol = "LBRA"; // solium-disable-line uppercase
-  uint8 public constant decimals = 18; // solium-disable-line uppercase
+    string public constant name = "LibraToken"; // solium-disable-line uppercase
+    string public constant symbol = "LBRA"; // solium-disable-line uppercase
+    uint8 public constant decimals = 18; // solium-disable-line uppercase
 
-  uint256 public constant INITIAL_SUPPLY = (10 ** 9) * (10 ** uint256(decimals));
+    uint256 public constant INITIAL_SUPPLY = (10 ** 9) * (10 ** uint256(decimals));
 
-  /**
-   * @dev Constructor that gives msg.sender all of existing tokens.
-   */
-  function LibraToken() public {
-    totalSupply_ = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
-    Transfer(0x0, msg.sender, INITIAL_SUPPLY);
-  }
+    /**
+    * @dev Constructor that gives msg.sender all of existing tokens.
+    */
+    function LibraToken() public {
+        totalSupply_ = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+        Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+    }
 
 }
